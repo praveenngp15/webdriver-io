39e6488724cbe64e74851263e7c802dec9bb24e1
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-04-11 01:06:44 +0530

    Section 8: Environments & Base URL (2) && Section 9: Logging

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,12 @@
-var baseUrl = 'http://www.webdriveruniversity.com'
+var baseUrl
+
+if (process.env.SERVER === 'prod') {
+    baseUrl = 'http://www.google.com'
+} else {
+    baseUrl = 'http://www.webdriveruniversity.com'
+}
 exports.config = {
-    
+
     //
     // ==================
     // Specify Test Files
@@ -78,7 +84,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl:baseUrl,
+    baseUrl: baseUrl,
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -112,7 +118,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-     services: ['selenium-standalone'],
+    services: ['selenium-standalone'],
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: http://webdriver.io/guide/testrunner/frameworks.html
@@ -170,7 +176,7 @@ exports.config = {
      */
     // beforeCommand: function (commandName, args) {
     // },
-    
+
     /**
      * Hook that gets executed before the suite starts
      * @param {Object} suite suite details
@@ -207,7 +213,7 @@ exports.config = {
      */
     // afterSuite: function (suite) {
     // },
-    
+
     /**
      * Runs after a WebdriverIO command gets executed
      * @param {String} commandName hook command name
