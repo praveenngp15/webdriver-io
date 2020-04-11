395ed4ec1af25944b9535ff8c1c97c28e53c8645
PraveenKumar Natarajan <praveenkumar.natarajan@flatironssolutions.com>
2020-04-11 20:51:36 +0530

    Section 16: Handling Browser Window Tabs

--- tests/webDriverUniversity.js
+++ tests/webDriverUniversity.js
@@ -1,30 +1,48 @@
 describe('Verify Whether webdriverUniversity link on homepage works correctly', function () {
 
     it('Check contactus button opens contact us page', function (done) {
-        browser
         browser.setViewportSize({
             width: 1200,
             height: 800
         })
+        this.timeout(30000)
         browser.url('/')
         var title = browser.getTitle()
         expect(title).to.equal('WebDriverUniversity.com')
         console.log('Title is: ' + title);
-        browser.debug();
+
         browser.click('#contact-us')
-        browser.pause(3000)
+        var tabIds = browser.getTabIds();
+        console.log(tabIds);
+        browser.switchTab(tabIds[1]);
+
+        var title2 = browser.getTitle();
+        expect(title2).to.equal('WebDriver | Contact Us');
+
+        var url = browser.getUrl();
+        expect(url).to.include('Contact-Us', 'URL Mismatch');
+        browser.close();
     })
 
 
 
     it('Check Login button opens Login portal page', function (done) {
         browser.url('/')
-        browser.click('#login-portal')
-        var title = browser.getTitle()
-        console.log('Title is: ' + title);
-        title.should.equal('WebDriverUniversity.com');  
-        browser.pause(3000)
+        this.timeout(30000)
 
+        var title = browser.getTitle();
+		title.should.equal('WebDriverUniversity.com');
+		console.log('Title is: ' + title);
+
+        browser.click('#login-portal')
+		var tabIds = browser.getTabIds();
+		browser.switchTab(tabIds[1]);
+        
+        var title2 = browser.getTitle();
+		expect(title2).to.equal('WebDriver | Login Portal');
+
+		var url = browser.getUrl();
+		expect(url).to.include('Login-Portal', 'URL Mismatch');
     })
 
 })
